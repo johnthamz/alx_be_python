1868751d2fa585cc94181fffcba0d77e67b560f5
johnthamz <johnthamz@gmail.com>
2025-09-14 14:23:35 +0300

    Update future_age_calculator.py and add finance_calculator.py

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,20 @@
+# Python script that will calculate the user’s monthly savings based on inputted monthly income and expenses
+#User Input for Financial Details
+    #Prompt the user for their monthly income
+
+monthly_income = int(input("Enter your monthly income"))
+total_monthly_expenses = int(input("Enter your total monthly expenses"))
+
+#Calculate Monthly Savings
+
+monthly_savings = monthly_income - total_monthly_expenses
+
+#Project Annual Savings
+#Assume a simple annual interest rate of 5%.
+#Calculate the projected savings after one year, incorporating the interest
+
+projected_savings = monthly_savings *12 + (monthly_savings * 12 * 0.05)
+
+#Display the results
+print(f"Your monthly savings are ${monthly_savings}")
+print(f"Projected savings after one year, with interest, is ${projected_savings}")
