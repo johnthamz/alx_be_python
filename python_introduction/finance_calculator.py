7437da9ba797a150af0a3d693685490a841b7e16
John Mithamo <johnthamz44@gmail.com>
2025-09-14 14:31:40 +0300

    Update finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -3,7 +3,7 @@
     #Prompt the user for their monthly income
 
 monthly_income = int(input("Enter your monthly income:"))
-total_monthly_expenses = int(input("Enter your total monthly expenses"))
+total_monthly_expenses = int(input("Enter your total monthly expenses:"))
 
 #Calculate Monthly Savings
 
